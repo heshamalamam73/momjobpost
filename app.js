afbc6ed3eb23d4516ea36c212c9b45647859b3b4
heshamalamam73 <heshamalamam73@gmail.com>
2018-05-11 02:53:45 +0200

    intial commit

---
+++ app.js
@@ -0,0 +1,39 @@
+var express     = require("express"),
+    app         = express(),
+    bodyParser  = require("body-parser"),
+    path        = require("path")
+
+
+
+
+app.set("view engine" , "ejs");
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static("public"));
+app.set('views', __dirname + '/views');
+
+
+
+
+
+
+app.get("/",function(req,res){
+    res.render("landing");
+});
+
+app.get("/about",function(req,res){
+    res.render("about");
+});
+app.get("/login", function(req,res){
+    
+    res.render("login");
+});
+
+
+
+
+
+
+app.listen(5000,function(){
+    console.log("the server started at 6000");
+});
